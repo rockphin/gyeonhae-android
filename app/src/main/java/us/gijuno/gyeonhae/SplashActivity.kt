9e56a239813381c27f865edb0f145bb9d95b479b
gijuno <itx.ljh.developer@gmail.com>
2021-07-24 14:11:45 +0900

    feat: make splash activity pass in 1.5sec

--- app/src/main/java/us/gijuno/gyeonhae/SplashActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/SplashActivity.kt
@@ -1,11 +1,28 @@
 package us.gijuno.gyeonhae
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        val hd = Handler()
+        hd.postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            this@SplashActivity.finish()
+        }, 1500)
+
     }
+
+    override fun onBackPressed() {
+
+    }
+
 }
