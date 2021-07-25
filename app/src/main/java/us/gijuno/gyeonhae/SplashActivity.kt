4f3b7938ce68514a04d378b4a8292581be809c39
gijuno <itx.ljh.developer@gmail.com>
2021-07-25 23:15:51 +0900

    fix: fix deprecated FULLSCREEN

--- app/src/main/java/us/gijuno/gyeonhae/SplashActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/SplashActivity.kt
@@ -1,10 +1,12 @@
 package us.gijuno.gyeonhae
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowInsets
 import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
@@ -12,7 +14,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("deprecation") // not deprecated in Q or below
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
 
         val hd = Handler(Looper.getMainLooper())
         hd.postDelayed({
