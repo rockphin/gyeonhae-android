86785c459e1f5b744b5bcd04ab1d79f4e34e208a
Beomjun Kim <nutyworks@gmail.com>
2021-07-26 20:55:45 +0900

    style: fix lint (#9)

--- app/src/androidTest/java/us/gijuno/gyeonhae/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/us/gijuno/gyeonhae/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package us.gijuno.gyeonhae
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
