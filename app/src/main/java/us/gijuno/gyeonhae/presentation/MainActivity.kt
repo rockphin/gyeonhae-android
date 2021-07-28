2182b8f90ff309ebbd5922b13715890aebd12302
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-07-29 07:31:54 +0900

    refactor: move activity to presentation layer (#10)

--- app/src/main/java/us/gijuno/gyeonhae/presentation/MainActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package us.gijuno.gyeonhae
+package us.gijuno.gyeonhae.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import us.gijuno.gyeonhae.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
