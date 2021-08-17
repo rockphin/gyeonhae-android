55358e81ae05e45517e89eccda6abfc84085fbb1
gijuno <itx.ljh.developer@gmail.com>
2021-08-17 17:57:00 +0900

    feat: add Title Fragment class file

---
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/title/MainTitleFragment.kt
@@ -0,0 +1,18 @@
+package us.gijuno.gyeonhae.presentation.title
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import us.gijuno.gyeonhae.R
+
+class MainTitleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_main_title, container, false)
+    }
+}
