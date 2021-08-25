9e21cb8fa48e3e5fe92e870bf2efb400b143a8e0
gijuno <itx.ljh.developer@gmail.com>
2021-08-26 08:22:43 +0900

    fix: delete unused package
    (cherry picked from commit 28efa74d1b3559154335f0593a32867e65e4d18f)

--- app/src/main/java/us/gijuno/gyeonhae/presentation/title/MainTitleFragment.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/title/MainTitleFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import us.gijuno.gyeonhae.R
 import us.gijuno.gyeonhae.databinding.FragmentMainTitleBinding
 import us.gijuno.gyeonhae.presentation.base.BaseFragment
