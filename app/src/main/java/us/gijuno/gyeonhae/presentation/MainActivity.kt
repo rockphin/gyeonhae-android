105fab7d76cd00c7717714bff2cba1ee60d2c4bd
gijuno <itx.ljh.developer@gmail.com>
2021-08-27 23:49:28 +0900

    style: fix code style what kotlin-lint wants

--- app/src/main/java/us/gijuno/gyeonhae/presentation/MainActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/MainActivity.kt
@@ -1,8 +1,6 @@
 package us.gijuno.gyeonhae.presentation
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
