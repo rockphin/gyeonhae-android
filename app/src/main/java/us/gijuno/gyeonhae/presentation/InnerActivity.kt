d093a92ecbaa5cfe6cdc403f146c3e616033019b
gijuno <itx.ljh.developer@gmail.com>
2021-08-30 12:09:08 +0900

    feat: build inner activity

---
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/InnerActivity.kt
@@ -0,0 +1,12 @@
+package us.gijuno.gyeonhae.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import us.gijuno.gyeonhae.R
+
+class InnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inner)
+    }
+}
