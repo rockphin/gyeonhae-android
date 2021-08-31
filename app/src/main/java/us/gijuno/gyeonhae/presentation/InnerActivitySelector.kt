f153980d4f249127cf71df2f9b265d005063aa80
gijuno <itx.ljh.developer@gmail.com>
2021-08-31 10:06:02 +0900

    style: code simplification

--- app/src/main/java/us/gijuno/gyeonhae/presentation/InnerActivitySelector.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/InnerActivitySelector.kt
@@ -1,13 +1,11 @@
 package us.gijuno.gyeonhae.presentation
 
-private var result: String = ""
 fun InnerActivitySelector(index: Int): String {
-    when (index) {
-        0 -> result = "recognize"
-        1 -> result = "convenience"
-        2 -> result = "setting"
-        3 -> result = "guide"
+    return when (index) {
+        0 -> "recognize"
+        1 -> "convenience"
+        2 -> "setting"
+        3 -> "guide"
+        else -> ""
     }
-
-    return result
 }
