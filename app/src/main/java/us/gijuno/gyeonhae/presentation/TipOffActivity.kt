75ad57add6847690b25941127c7f784c4b620644
gijuno <itx.ljh.developer@gmail.com>
2021-09-06 16:04:28 +0900

    feat: implement webView

--- app/src/main/java/us/gijuno/gyeonhae/presentation/TipOffActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/TipOffActivity.kt
@@ -2,11 +2,26 @@ package us.gijuno.gyeonhae.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import us.gijuno.gyeonhae.R
 
 class TipOffActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tip_off)
+        val tipOffWebview = findViewById<WebView>(R.id.tip_off_webViews)
+
+        tipOffWebview.webViewClient = WebViewClient()
+        tipOffWebview.webChromeClient = WebChromeClient()
+
+        tipOffWebview.apply {
+            settings.useWideViewPort = true
+            settings.setSupportZoom(true)
+            settings.builtInZoomControls = true
+        }
+
+        tipOffWebview.loadUrl("https://forms.gle/FrnvgCuRT3NzcSZJ6")
     }
 }
