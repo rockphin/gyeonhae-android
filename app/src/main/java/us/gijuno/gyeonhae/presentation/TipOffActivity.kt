e50ae29ab1f0ee9ff5cdc35c2d9a3974e5004be1
gijuno <itx.ljh.developer@gmail.com>
2021-09-06 10:55:22 +0900

    feat: build tip off activity

---
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/TipOffActivity.kt
@@ -0,0 +1,12 @@
+package us.gijuno.gyeonhae.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import us.gijuno.gyeonhae.R
+
+class TipOffActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tip_off)
+    }
+}
