c998f0de6a01d35ad0db4ca763df19dc2d64d253
gijuno <itx.ljh.developer@gmail.com>
2021-09-06 16:08:10 +0900

    style: fix code style with kotlin-lint

--- app/src/main/java/us/gijuno/gyeonhae/presentation/TipOffActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/TipOffActivity.kt
@@ -1,10 +1,10 @@
 package us.gijuno.gyeonhae.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import us.gijuno.gyeonhae.R
 
 class TipOffActivity : AppCompatActivity() {
