b0a744b931136a2805283eb074e06e522038646c
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-09-08 23:43:21 +0900

    feat: vibrate function (#56)
    * feat: add vibrate permission
    
    * feat: make one and repeat vibrate function
    
    * style: kotlin-lint
    
    * style: change function name

---
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/GyeonHaeApplication.kt
@@ -0,0 +1,34 @@
+package us.gijuno.gyeonhae.presentation
+
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+
+class GyeonHaeApplication : Application() {
+    private var _vibrator: Vibrator? = null
+    private val vibrator: Vibrator
+        get() {
+            if (_vibrator == null) {
+                _vibrator = this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            }
+            return _vibrator!!
+        }
+
+    fun vibrateOnce(time: Long, amplitude: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(time, amplitude))
+        } else {
+            vibrator.vibrate(time)
+        }
+    }
+
+    fun vibrateRepeat(pattern: LongArray) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createWaveform(pattern, -1))
+        } else {
+            vibrator.vibrate(pattern, -1)
+        }
+    }
+}
