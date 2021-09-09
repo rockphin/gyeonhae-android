c7351069e77651402ada3cf9abc73388a71f9c7d
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-09-09 15:43:15 +0900

    barcode-result-retrofit (#62)
    * feat: implementation retrofit2
    
    * feat: draw layout (#61)
    
    (cherry picked from commit cf1592a59a2bdbb05494dfe357cb6fa6ee431bc1)
    
    * feat: show barcode result

---
+++ app/src/main/java/us/gijuno/gyeonhae/data/ServiceRequester.kt
@@ -0,0 +1,20 @@
+package us.gijuno.gyeonhae.data
+
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
+
+object ServiceRequester {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://gyeonhae-server-rcbgcoutpa-du.a.run.app/recognize/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val service = retrofit.create(Service::class.java)
+
+    fun getBarcode(code: String): String {
+        return service.getBarcode(code).execute().body() ?: throw IllegalArgumentException()
+    }
+}
