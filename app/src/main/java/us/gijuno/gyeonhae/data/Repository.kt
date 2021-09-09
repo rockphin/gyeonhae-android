3ec39e9d9d45ed381812278344cc1f48112d3474
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-09-09 21:27:32 +0900

    feat: barcode activity retrofit (#64)

--- app/src/main/java/us/gijuno/gyeonhae/data/Repository.kt
+++ app/src/main/java/us/gijuno/gyeonhae/data/Repository.kt
@@ -2,20 +2,27 @@ package us.gijuno.gyeonhae.data
 
 import android.app.Application
 import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import us.gijuno.gyeonhae.presentation.GyeonHaeApplication
 import java.lang.Exception
 
 class Repository {
-    var barcodeResult: String = ""
-    fun getBarcode(code: String) {
+    var barcodeResult: String = "err"
+    suspend fun getBarcode(code: String) {
         try {
-            ServiceRequester.getBarcode(code).let {
-                barcodeResult = it
+            withContext(Dispatchers.IO) {
+                ServiceRequester.getBarcode(code).let {
+                    Log.d("camerax", "List<Barcode>: $it")
+                    withContext(Dispatchers.Main) {
+                        Log.d("camerax", "productName: ${it[0].productName}")
+                        barcodeResult = it[0].productName
+                    }
+                }
             }
         } catch (e: Exception) {
             e.printStackTrace()
-            barcodeResult = e.toString()
-            Log.d("Repository", "Barcode Result: $e")
+            Log.e("Repository", "Barcode Result: $e")
         }
     }
 }
