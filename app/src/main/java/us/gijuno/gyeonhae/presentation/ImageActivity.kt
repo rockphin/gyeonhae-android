20f6c90af6a0f786c0925caa740cc37d874cb20c
Gijuno <46197097+Gijuno@users.noreply.github.com>
2021-09-09 21:27:18 +0900

    feat: can save image in ImageActivity (#63)

--- app/src/main/java/us/gijuno/gyeonhae/presentation/ImageActivity.kt
+++ app/src/main/java/us/gijuno/gyeonhae/presentation/ImageActivity.kt
@@ -2,80 +2,61 @@ package us.gijuno.gyeonhae.presentation
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.Image
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.zxing.BinaryBitmap
 import com.google.zxing.MultiFormatReader
-import com.google.zxing.NotFoundException
-import com.google.zxing.PlanarYUVLuminanceSource
-import com.google.zxing.common.HybridBinarizer
 import us.gijuno.gyeonhae.R
 import us.gijuno.gyeonhae.data.Repository
 import java.io.File
-import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-typealias BarcodeListener = (result: String) -> Unit
-
-class BarcodeActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity() {
     private var imageCapture: ImageCapture? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var preview: Preview
-    private lateinit var imageAnalyzer: ImageAnalysis
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_barcode)
+        setContentView(R.layout.activity_image)
 
         preview = Preview.Builder()
             .build()
-            .also { it.setSurfaceProvider(findViewById<PreviewView>(R.id.barcodeViewFinder).surfaceProvider) }
+            .also { it.setSurfaceProvider(findViewById<PreviewView>(R.id.sceneViewFinder).surfaceProvider) }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
 
-        imageAnalyzer = ImageAnalysis.Builder()
-            .build()
-            .also {
-                it.setAnalyzer(cameraExecutor, BarcodeAnalyzer(MultiFormatReader()) { result ->
-                    Log.d(TAG, "Result: $result")
-                    (application as GyeonHaeApplication).scanSuccess()
-                    findViewById<TextView>(R.id.barcode_result).text = Repository().barcodeResult
-                    Log.d(TAG, "Response: ${Repository().barcodeResult}")
-                    stopCamera()
-                })
-            }
+        outputDirectory = getOutputDirectory()
 
-        // Request camera permissions
+        findViewById<ImageView>(R.id.image_take_image_btn).setOnClickListener { takePhoto() }
+
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
-                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS)
         }
-
-        // Set up the listener for take photo button
-//        findViewById<ImageView>(R.id.camera_capture_button).setOnClickListener { takePhoto() }
-
-        outputDirectory = getOutputDirectory()
     }
 
     private fun takePhoto() {
@@ -114,8 +95,6 @@ class BarcodeActivity : AppCompatActivity() {
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        (application as GyeonHaeApplication).scanStart()
-
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
@@ -132,7 +111,7 @@ class BarcodeActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
+                    this, cameraSelector, preview, imageCapture)
             } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
@@ -146,13 +125,8 @@ class BarcodeActivity : AppCompatActivity() {
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            cameraProvider.unbind(preview, imageAnalyzer)
+            cameraProvider.unbind(preview)
         }, ContextCompat.getMainExecutor(this))
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onRequestPermissionsResult(
@@ -182,9 +156,9 @@ class BarcodeActivity : AppCompatActivity() {
             mediaDir else filesDir
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     companion object {
@@ -192,44 +166,5 @@ class BarcodeActivity : AppCompatActivity() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-    }
-
-    private class BarcodeAnalyzer(
-        private val formatReader: MultiFormatReader,
-        private val listener: BarcodeListener,
-    ) : ImageAnalysis.Analyzer {
-
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        override fun analyze(image: ImageProxy) {
-            val data = image.planes[0].buffer.toByteArray()
-
-            Log.d(TAG, "w ${image.width}, h ${image.height}, r ${image.imageInfo.rotationDegrees}")
-
-            val source = PlanarYUVLuminanceSource(
-                data,
-                image.width,
-                image.height,
-                0,
-                0,
-                image.width,
-                image.height,
-                false
-            )
-
-            val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-
-            try {
-                val result = formatReader.decode(binaryBitmap)
-                listener(result.text)
-            } catch (e: NotFoundException) {
-            }
-            image.close()
-        }
     }
 }
